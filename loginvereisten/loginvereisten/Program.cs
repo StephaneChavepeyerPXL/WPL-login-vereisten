1ddbb985fe92e57bb4d9fb410de7d45ccde7c067
Stephane Chavepeyer <stephane.chavepeyer@student.pxl.be>
2025-11-06 13:13:55 +0100

    DEMO-01

---
+++ loginvereisten/loginvereisten/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace loginvereisten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username;
+
+            Console.Write("Gebruikersnaam: ");
+            username = Console.ReadLine();
+            
+        }
+    }
+}
